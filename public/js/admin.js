bcdaad425c9d0a045cfb53932263cfd2f60422d6
Vsevolod Romashov <7@7vn.ru>
2012-06-18 15:51:40 +0400

    correct button output

--- public/js/admin.js
+++ public/js/admin.js
@@ -2,7 +2,15 @@ var admin = {
   render: function() {
     var last_button = $('#add_day');
     $(settings.schedule).each(function(index, day) {
-      var button_html = '<input class="btn" type="button" value="' + day + '">';
+      var button_html;
+      if(day == 0) {
+        // выходной
+        button_html = '<input class="btn" type="button" value="' + (index + 1) + '">';
+      } else {
+        // рабочий
+        button_html = '<input class="btn btn-inverse" type="button" value="' + (index + 1) + '">';
+      }
+      
       $(button_html).insertBefore(last_button);
     });
   },
