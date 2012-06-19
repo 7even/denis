89dfc0a65064d46ecf93b96cc1874ac1eb759f41
Vsevolod Romashov <7@7vn.ru>
2012-06-19 06:33:22 +0400

    schedule editing and saving

--- app.rb
+++ app.rb
@@ -6,11 +6,17 @@ get '/' do
   slim :index
 end
 
+get '/admin' do
+  slim :admin
+end
+
 get '/settings.json' do
   content_type :json
   {settings: Setting.to_hash, vacations: Vacation.to_hash}.to_json
 end
 
-get '/admin' do
-  slim :admin
+post '/settings.json' do
+  Setting.schedule = params[:days]
+  content_type :json
+  params.to_json
 end
--- public/js/admin.js
+++ public/js/admin.js
@@ -1,14 +1,40 @@
 var admin = {
+  addDay: function() {
+    this.days.push(0);
+    this.render();
+  },
+  
+  removeDay: function() {
+    this.days.pop();
+    this.render();
+  },
+  
+  toggleDay: function(day_index) {
+    if(this.days[day_index] == 0) {
+      this.days[day_index] = 1;
+    } else if(this.days[day_index] == 1) {
+      this.days[day_index] = 0;
+    }
+    
+    this.render();
+  },
+  
+  clear: function() {
+    $('.day').remove();
+  },
+  
   render: function() {
+    this.clear();
     var last_button = $('#add_day');
-    $(settings.schedule).each(function(index, day) {
+    
+    $(this.days).each(function(index, day) {
       var button_html;
       if(day == 0) {
         // выходной
-        button_html = '<input class="btn" type="button" value="' + (index + 1) + '">';
+        button_html = '<input id="' + index + '" class="day btn" type="button" value="' + (index + 1) + '">';
       } else {
         // рабочий
-        button_html = '<input class="btn btn-inverse" type="button" value="' + (index + 1) + '">';
+        button_html = '<input id="' + index + '" class="day btn btn-inverse" type="button" value="' + (index + 1) + '">';
       }
       
       $(button_html).insertBefore(last_button);
@@ -17,7 +43,16 @@ var admin = {
   
   loadAndRender: function() {
     settings.load(function() {
+      admin.days = settings.schedule;
       admin.render();
     });
+  },
+  
+  save: function() {
+    var data = {
+      days: this.days
+    };
+    
+    $.post('/settings.json', data);
   }
 };
